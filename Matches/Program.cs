b5391e03395b8d09a2ff30352b703ac6bb5f4022
PinchukVitaliy@ukr.net <PinchukVitaliy@ukr.net>
2019-01-10 22:45:36 +0200

    Two

--- Matches/Program.cs
+++ Matches/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Matches
 {
@@ -10,6 +6,33 @@ namespace Matches
     {
         static void Main(string[] args)
         {
-        }
+            int k;      // matches    
+            int l;
+
+            int a = 1; // side
+            Console.WriteLine("Enter the number of squares: ");
+            int n = int.Parse(Console.ReadLine());   
+            if(1 > n || n > 1000000000)
+            {
+                Console.WriteLine("The number of squares must be greater than 0 but less than 1,000,000,000");
+                return;
+            }
+            while ((a + 1) * (a + 1) < n)
+            {
+                a = a + 1;             
+            }
+            k = 2 * (a * a + a);
+            l = n - a * a;
+            if (l > 0)
+            {
+                k = k + 2 * l + 1;
+            }
+            if (l > a)
+            {
+                k = k + 1;
+            }
+            Console.WriteLine("Minimum number of matches required: {0}", k);
+            Console.ReadKey();
+        }        
     }
 }
